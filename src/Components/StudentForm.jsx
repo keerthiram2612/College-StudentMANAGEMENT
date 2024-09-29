b71c183732c6d94ed6f4a0517312c182e3e528e2
Keerthana <keerthiram2612@gmail.com>
2024-09-29 14:09:09 +0530

    updating the student form

--- src/Components/StudentForm.jsx
+++ src/Components/StudentForm.jsx
@@ -2,163 +2,92 @@ import React, { useState } from 'react';
 import "../Pages/StudentForm.css";
 
 const StudentForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    rollNumber: '',
-    department: '',
-    yearOfStudy: '',
-    email: '',
-    phone: '',
-    photo: null,  // For storing the uploaded photo
-  });
-
-  const [photoPreview, setPhotoPreview] = useState(null);  // For showing the preview
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  // Handle photo upload separately
-  const handlePhotoChange = (e) => {
-    const file = e.target.files[0];
-    setFormData({
-      ...formData,
-      photo: file,
-    });
-    setPhotoPreview(URL.createObjectURL(file)); // Generate a preview URL
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // You can append the form data and the photo to FormData to send to the backend
-    const data = new FormData();
-    Object.keys(formData).forEach(key => {
-      data.append(key, formData[key]);
-    });
-
-    console.log('Student Data Submitted:', formData);
-    // API call to send 'data' to the server
-
-    // For now, resetting form
-    setFormData({
-      name: '',
-      rollNumber: '',
-      department: '',
-      yearOfStudy: '',
-      email: '',
-      phone: '',
-      photo: null,
-    });
-    setPhotoPreview(null);  // Clear photo preview
-  };
 
   return (
-    <div className='student-image'>
-        <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-
-      <div>
-        <label htmlFor="rollNumber">Roll Number:</label>
-        <input
-          type="text"
-          id="rollNumber"
-          name="rollNumber"
-          value={formData.rollNumber}
-          onChange={handleChange}
-          required
-        />
-      </div>
-
-      <div>
-        <label htmlFor="department">Department:</label>
-        <input
-          type="text"
-          id="department"
-          name="department"
-          value={formData.department}
-          onChange={handleChange}
-          required
-        />
-      </div>
-
-      <div>
-        <label htmlFor="yearOfStudy">Year of Study:</label>
-        <select
-          id="yearOfStudy"
-          name="yearOfStudy"
-          value={formData.yearOfStudy}
-          onChange={handleChange}
-          required
-        >
-          <option value="">Select Year</option>
-          <option value="1">1st Year</option>
-          <option value="2">2nd Year</option>
-          <option value="3">3rd Year</option>
-          <option value="4">4th Year</option>
-        </select>
-      </div>
-
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-
-      <div>
-        <label htmlFor="phone">Phone:</label>
-        <input
-          type="tel"
-          id="phone"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          required
-        />
-      </div>
-
-      <div>
-        <label htmlFor="photo">Upload Photo:</label>
-        <input
-          type="file"
-          id="photo"
-          name="photo"
-          accept="image/*"
-          onChange={handlePhotoChange}
-        />
-        {photoPreview && (
-          <div>
-            <img
-              src={photoPreview}
-              alt="Preview"
-              style={{ width: '150px', height: '150px', objectFit: 'cover', marginTop: '10px' }}
-            />
-          </div>
-        )}
-      </div>
-
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <div className='student-image'></div>
+       <div className='back'>
+       <div className='container student-form'>
+        <div className='form'>
+            <form action="text-center">
+              <div className="row">
+              <div className="col-md-6">
+              <div className="left">
+                <label>Name:</label><input type='text'></input><br/>
+                <label>Email:</label><input type='text'></input><br/>
+                <label>Password:</label><input type='text'></input>
+                <div className='birthday'>
+                  <label for="birthday">Birthday:</label>
+                 <input type="date" id="birthday" name="birthday"/>
+                 </div>
+                 <div className='gender'>
+                 <label >Gender:</label>
+                <input type='radio' placeholder='Male'>
+                </input>
+                <label>Male</label>
+                 <input type='radio' placeholder='Male'>
+                 </input>
+                 <label>Female</label>
+                 </div>
+                <div className=''>
+                <label>Roll No:</label><input type='text'></input>
+                </div>
+                <div className='department'>
+                  <label>Department:</label>
+                  <select>
+                    <option value={""}>Computer Science</option>
+                    <option value={""}>Information Technology</option>
+                    <option value={""}>Textile</option>
+                   </select>
+                </div>
+                <div className='year'>
+                  <label>Year:</label>
+                  <select>
+                    <option value={""}>Select year</option>
+                    <option value={""}>1</option>
+                    <option value={""}>2</option>
+                    <option value={""}>3</option>
+                    <option value={""}>4</option>
+                   </select>
+                </div>
+                 <label>Phone:</label><input type="text" />
+                 <div className='passing-year'>
+                  <label>Passing Year:</label>
+                  <select>
+                    <option value={""}> year</option>
+                    <option value={""}>2023</option>
+                    <option value={""}>2024</option>
+                    <option value={""}>2025</option>
+                    <option value={""}>2026</option>
+                   </select>
+                </div>
+                <label>Aadhar:</label><input type='text'></input><br />
+                <label>Father Name:</label><input type='text'></input><br />
+              </div>
+              </div>
+              <div className="col-md-6">
+                   <div className="right ">
+                   <label>Upload Image:</label><input type='file'></input>
+                  <label>Mother Name:</label><input type='text'/><br />
+                   <label>Community</label>
+                   <select>
+                    <option>Select</option>
+                    <option>Mbc</option>
+                    <option>Bc</option>
+                    <option>Oc</option>
+                    <option>Sc/St</option>
+                   </select><br />
+                   <label>Address:</label><input className='add' type='text'/><br />
+                   <div className=''>
+                   <button className='submit'>Submit</button>
+                   </div>
+                   </div>
+                </div>
+              </div>
+            </form>
+        </div>
+       </div>
+       </div>
     </div>
   );
 };
